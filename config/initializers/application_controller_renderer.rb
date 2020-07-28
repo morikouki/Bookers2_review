435392697aed88ff927b96ceeb3ade575cda7d5d
vagrant <akzouksia@icloud.com>
2020-07-28 09:29:33 +0000

    [Add]review

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0e6ce4707a8ff7b37887dcfc7f34b3dbc781863627fac0e0668e53bd088c4ef34699411285a05a40c7992513951916fb94b0ebb4401cc3e4a9747a6f58e0b0fb'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+
+  devise_for :users
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
