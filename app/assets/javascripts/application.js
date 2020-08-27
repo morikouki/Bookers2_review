d62f9ebb791680f0c606a9ac4a01c7a40c725358
vagrant <akzouksia@icloud.com>
2020-08-27 10:23:23 +0000

    [Finish]ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,41 +10,24 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require bootstrap-sprockets
 //= require activestorage
 //= require rails-ujs
 //= require turbolinks
-//= require jquery
-//= require bootstrap-sprockets
 //= require_tree .
-var title = "javascriptが使えました";
-alert(title);
-$(document).ready(function () {
-  $('.jquery').on('click', function(){
-    $(this).css('color','red');
-  });
-});
 
-$(document).ready(function () {
-  $("#theTarget").skippr({
-    // スライドショーの変化 ("fade" or "slide")
-    transition : 'slide',
-    // 変化に係る時間(ミリ秒)
-    speed : 1000,
-    // easingの種類
-    easing : 'easeOutQuart',
-    // ナビゲーションの形("block" or "bubble")
-    navType : 'block',
-    // 子要素の種類("div" or "img")
-    childrenElementType : 'div',
-    // ナビゲーション矢印の表示(trueで表示)
-    arrows : true,
-    // スライドショーの自動再生(falseで自動再生なし)
-    autoPlay : true,
-    // 自動再生時のスライド切替間隔(ミリ秒)
-    autoPlayDuration : 3000,
-    // キーボードの矢印キーによるスライド送りの設定(trueで有効)
-    keyboardOnAlways : true,
-    // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
-    hidePrevious : false
+$(function() {
+  $(document).on('turbolinks:load', () => {
+    $('#user_postal_code').jpostal({
+      postcode : [
+        '#user_postal_code'
+      ],
+      address: {
+        "#user_prefecture_code": "%3", // # 都道府県が入力される
+        "#user_city"           : "%4%5", // # 市区町村と町域が入力される
+        "#user_street"         : "%6%7" // # 大口事務所の番地と名称が入力される
+      }
+    });
   });
 });
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-  end
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :postal_code, :prefecture_code, :city, :street])
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:name]) # ログイン時にnameを使用
+    end
 	
 end
