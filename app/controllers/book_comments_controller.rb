e421ccaf1b91fc12dc7a66758ce665138756572b
vagrant <akzouksia@icloud.com>
2020-11-02 13:40:23 +0900

    [Finish]BookComment_ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,14 +5,12 @@ class BookCommentsController < ApplicationController
 		comment = current_user.book_comments.new(book_comment_params)
 		comment.book_id = @book.id
 		comment.save
-		redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
 		@book = Book.find(params[:book_id])
 		comment = current_user.book_comments.find_by(book_id: @book.id)
 		comment.destroy
-		redirect_back(fallback_location: root_path)
 	end
 
 	private
