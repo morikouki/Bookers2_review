2f084d37d12e7ef83b81bbe3a9718975601e7473
vagrant <akzouksia@icloud.com>
2020-11-03 15:12:45 +0900

    [Finish Relationship]

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -11,8 +11,12 @@ class RelationshipsController < ApplicationController
 	end
 
 	def follower
+		user = User.find(params[:user_id])
+		@users = user.follower_user
 	end
 
 	def following
+		user = User.find(params[:user_id])
+		@users = user.following_user
 	end
 end
